362c8ed0eddbba68ee74014792577fb02ebb842f
Mark Bucciarelli <mkbucc@gmail.com>
2021-09-06 08:18:01 -0400

    A number of fixes to the test runner
      * Don't redirect the server, started in the background,
        and curl to the same file.  Two processes that are
        running at the same time and redirecting their stdout
        to the same file can product unreliable results.
      * Clean dates and local ports that can vary from test run
        to test run out of the test output before comparing with
        the expected (gold) files.
      * Don't delete the actual test output.  This is useful to
        have when we have changed behavior, as the actual output
        can be used to update the gold standard.

--- test/runtest.sh
+++ test/runtest.sh
@@ -7,14 +7,31 @@ name="$2"
 route="$3"
 
 fn=test$n
+logfile=test/$fn.out
+
+# rmdates removes datetime strings that change with every test run.
+rmdates() {
+  sed -e 's/^.\{23\} 2[0-9]\{3\}/<Date.toString()>/' -e 's/^Date:.*/Date:/'
+}
+
+# rmports removes local port strings that change with every test run.
+rmports() {
+  sed 's/\(remote=[^:]*\):[1-9][0-9][0-9]*/\1:<localport>/'
+}
+
 
 ./stop.sh > /dev/null
 sleep 0.5
-./start.sh 9876 > test/$fn.out
+./start.sh 9876 >test/server.out 2>&1
 sleep 0.5
-curl -D- -sS http://127.0.0.1:9876${route} \
-  |sed 's/^Date:.*/Date:/'>> test/$fn.out
-printf "%-65s" "$name"
-diff -uw test/$fn.gold test/$fn.out >&2 && echo PASS || echo FAIL
+
+curl -D- -sS http://127.0.0.1:9876${route} >test/curl.out 2>&1
+
 ./stop.sh > /dev/null
-rm -f test.out
+sleep 0.5
+
+cat test/server.out test/curl.out | rmdates | rmports > $logfile
+
+printf "%-65s" "$name"
+diff -uw test/$fn.gold $logfile >&2 && echo PASS || echo FAIL
+
