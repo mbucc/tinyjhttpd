bfe923d4deb66b8c9c519a7cad41da2d963df953
Mark Bucciarelli <mkbucc@gmail.com>
2021-09-08 19:22:22 -0400

    Read client request in HelloWorld handler example
    This was the cause of ab stopping after ~16,000 requests.  Based
    on two [1,2] stack overflow posts, I believe that:
    
      1. the server closed the connection
    
      2. ab opened a new connection (which opened a new client port),
    
      3. OSX ran out of "ephemeral ports"
    
    Per [2],
    
        The default ephemeral port range on osx is 49152-65535,
        which is only 16,383 ports.
        ...
        As each port is used, it gets put into a queue where
        it waits for the tcp "Maximum Segment Lifetime", which
        is configured to be 15 seconds on osx.  So if you use
        more than 16,383 ports in 15 seconds, you're effectively
        going to get throttled by the OS on further connections.
        Depending on which process runs out of ports first, you
        will get connection errors from the server, or hangs
        from ab.
    
    [1] https://stackoverflow.com/questions/11980748/java-http-request-response-from-socket-server-closing-output-stream-before-clie
    
    [2] https://stackoverflow.com/a/30357879/1789168

--- src/com.example.helloworld/com/example/helloworld/HelloWorld.java
+++ src/com.example.helloworld/com/example/helloworld/HelloWorld.java
@@ -5,6 +5,8 @@ import com.markbucciarelli.tinyjhttpd.HandlerResponse;
 import com.markbucciarelli.tinyjhttpd.types.HTTPHeader;
 import com.markbucciarelli.tinyjhttpd.types.HTTPStatus;
 import com.sun.net.httpserver.HttpExchange;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
@@ -17,10 +19,19 @@ public class HelloWorld extends BaseHTTPHandler {
 
   @Override
   public HandlerResponse safeHandle(HttpExchange x) {
+    readRequest(x);
     return new HandlerResponse(
       HTTPStatus.OK,
       "Hello World".getBytes(StandardCharsets.UTF_8),
       Arrays.asList(HTTPHeader.cacheDays(365), HTTPHeader.plaintext())
     );
+  }
+
+  private void readRequest(HttpExchange x) {
+    try {
+      x.getRequestBody().transferTo(OutputStream.nullOutputStream());
+    } catch (IOException e) {
+      throw new IllegalStateException("can't read request", e);
+    }
   }
 }
